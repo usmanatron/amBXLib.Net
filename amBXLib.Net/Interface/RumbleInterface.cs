263c469375454bab8abdef3eea2fd194a700c321
Usman Iqbal <usman.iqbal@softwire.com>
2018-11-27 12:52:26 +0000

    Add some "DONT TOUCH" comments

--- amBXLib.Net/Interface/RumbleInterface.cs
+++ amBXLib.Net/Interface/RumbleInterface.cs
@@ -3,6 +3,9 @@ using System.Runtime.InteropServices;
 
 namespace amBXLib.Net.Interface
 {
+  /// <remarks>
+  /// The order MUST NOT BE CHANGED; it needs to match that in the C class.
+  /// </remarks>
   [StructLayout(LayoutKind.Sequential)]
   public struct RumbleInterface
   {
